c09e3d02c3fdd97f91c22ab4cae638688609e743
小小孩子们 <2595557847@qq.com>
2023-03-20 00:43:06 +0800

    优化速度

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  modules: ['@element-plus/nuxt'],
   experimental: {
     externalVue: true,
   },
   css: ['@/assets/css/app.less', '@/assets/css/color.less'],
   plugins: [
     {
-      src: '~/plugins/element-plus.js',
-    },
-    {
       src: '~/plugins/vant.js',
     },
   ],
+  sourcemap: {
+    server: false,
+    client: false,
+  },
 })
