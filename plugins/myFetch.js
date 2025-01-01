d1164d0bfd5b0915762328d2aea2d358dd5a3af3
xiaoxiao <2595557847@qq.com>
2025-01-01 11:10:01 +0800

    优化首页样式

--- plugins/myFetch.js
+++ plugins/myFetch.js
@@ -26,7 +26,8 @@ export default defineNuxtPlugin((/* nuxtApp */) => {
           obj.headers.Authorization = authorization.value
         }
 
-        const baseURL = 'https://xxapi.cn/api/'
+        // const baseURL = 'https://xxapi.cn/api/'
+        const baseURL = 'http://192.168.31.98:3005/admin/'
         // const baseURL = 'http://10.6.6.6:3005/admin/'
 
         return $fetch(baseURL + request, { ...opts, ...obj })
