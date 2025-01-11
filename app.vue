c11f6f1a37f906539e8ba521b3fbfa5262fd9e53
xiaoxiao <2595557847@qq.com>
2025-01-11 15:13:49 +0800

    添加全局配置，将element设置为中文

--- app.vue
+++ app.vue
@@ -1,9 +1,15 @@
 <template>
-  <NuxtPage></NuxtPage>
+  <el-config-provider :locale="zhCn">
+    <NuxtPage></NuxtPage>
+  </el-config-provider>
   <el-backtop :right="100" :bottom="100" />
 </template>
 
 <script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 添加全局配置，设置中文
+const locale = ref(zhCn)
+
 const token = useCookie('token')
 
 const authorization = useState('Authorization')
@@ -17,7 +23,7 @@ useHead({
   ],
   script: [
     {
-      src: 'https://hm.baidu.com/hm.js?277dc1783564930b9da342049f6ddea9',
+      // src: 'https://hm.baidu.com/hm.js?277dc1783564930b9da342049f6ddea9',
     },
   ],
 })
